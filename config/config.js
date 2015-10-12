8df033be1c785de4647b74995acdd8a0f1421b7f
liudonghua123 <liudonghua123@gmail.com>
2015-10-12 14:12:55 +0800

    fix redis connnection error

--- config/config.js
+++ config/config.js
@@ -16,7 +16,7 @@ var config = {
         },
         redis: {
             host: process.env.REDIS_HOST || 'localhost',
-            port: process.env.REDIS_PORT || 6379
+            port: process.env.REDIS_PORT_6379_TCP_PORT || 6379
         },
         webservice: {
             url: process.env.WEBSERVICE_URL,
@@ -38,7 +38,7 @@ var config = {
         },
         redis: {
             host: process.env.REDIS_HOST || 'redis',
-            port: process.env.REDIS_PORT || 6379
+            port: process.env.REDIS_PORT_6379_TCP_PORT || 6379
         },
         webservice: {
             url: process.env.WEBSERVICE_URL,
